3786b7ce4922a56030fbe0eb70b1cd7ff411086f
ancorframe <yura.pundiak@gmail.com>
2022-10-06 20:14:02 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,3 @@
-import { SocialProfile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-
-import user from '../user';
-import data from '../data';
-import friends from '../friends';
-import transaction from '../transactions';
-import { Box } from './Box';
 
 export const App = () => {
   return (
@@ -19,10 +9,6 @@ export const App = () => {
       gridGap={5}
       pt={5}
     >
-      <SocialProfile user={user} />
-      <Statistics data={data} title={"Uploads Stats"} />
-      <FriendList friends={friends} />
-      <TransactionHistory transaction={transaction} />
     </Box>
   );
 };
