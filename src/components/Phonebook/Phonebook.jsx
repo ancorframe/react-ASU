2fc3df856ce2af8be3831bd0911fdd097744216a
ancorframe <yura.pundiak@gmail.com>
2022-10-21 15:41:33 +0300

    +

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,69 +1,114 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
 import { Filter } from './Filter';
 import { Title } from './Phonebook.styled';
 
-export class Phonebook extends React.Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const Phonebook = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
 
-  formSubmit = data => {
+  const formSubmit = data => {
     const id = nanoid();
     const obj = { ...data, id };
-    this.setState(prevState => ({
-      contacts: prevState.contacts.concat([obj]),
-    }));
+
+    setContacts(prev => [...prev, obj]);
   };
 
-  filterByName = () => {
-    return this.state.contacts.filter(i => {
-      return i.name.toLowerCase().includes(this.state.filter);
+  const filterByName = () => {
+    return contacts.filter(i => {
+      return i.name.toLowerCase().includes(filter);
     });
   };
 
-  filter = data => {
-    this.setState({ filter: data });
+  const filterValue = data => {
+    setFilter(data);
   };
 
-  onDelete = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(i => i.id !== id.target.id),
-    }));
+  const onDelete = id => {
+    setContacts(prev => prev.filter(i => i.id !== id.target.id));
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsetContacts = JSON.parse(contacts);
-    if (parsetContacts) {
-      this.setState({ contacts: parsetContacts });
-    }
-  }
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  return (
+    <>
+      <Title>Phonebook </Title>
+      <ContactForm onSubmit={formSubmit} contacts={contacts} />
+      <Title>Contacts</Title>
+      <Filter valueIn={filter} valueOut={filterValue} />
+      <ContactList
+        list={filter ? filterByName() : contacts}
+        onClick={onDelete}
+      />
+    </>
+  );
+};
 
-  render() {
-    return (
-      <>
-        <Title>Phonebook </Title>
-        <ContactForm
-          onSubmit={this.formSubmit}
-          contacts={this.state.contacts}
-        />
-        <Title>Contacts</Title>
-        <Filter valueIn={this.state.filter} valueOut={this.filter} />
-        <ContactList
-          list={this.state.filter ? this.filterByName() : this.state.contacts}
-          onClick={this.onDelete}
-        />
-      </>
-    );
-  }
-}
+// export class Phonebook extends React.Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
+
+//   formSubmit = data => {
+//     const id = nanoid();
+//     const obj = { ...data, id };
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.concat([obj]),
+//     }));
+//   };
+
+//   filterByName = () => {
+//     return this.state.contacts.filter(i => {
+//       return i.name.toLowerCase().includes(this.state.filter);
+//     });
+//   };
+
+//   filter = data => {
+//     this.setState({ filter: data });
+//   };
+
+//   onDelete = id => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(i => i.id !== id.target.id),
+//     }));
+//   };
+
+//   componentDidMount() {
+//     const contacts = localStorage.getItem('contacts');
+//     const parsetContacts = JSON.parse(contacts);
+//     if (parsetContacts) {
+//       this.setState({ contacts: parsetContacts });
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <Title>Phonebook </Title>
+//         <ContactForm
+//           onSubmit={this.formSubmit}
+//           contacts={this.state.contacts}
+//         />
+//         <Title>Contacts</Title>
+//         <Filter valueIn={this.state.filter} valueOut={this.filter} />
+//         <ContactList
+//           list={this.state.filter ? this.filterByName() : this.state.contacts}
+//           onClick={this.onDelete}
+//         />
+//       </>
+//     );
+//   }
+// }
