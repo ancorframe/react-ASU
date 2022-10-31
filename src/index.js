d069dbdb3fb3e396b941709d0f9f91bc08b168bd
ancorframe <yura.pundiak@gmail.com>
2022-10-31 22:27:57 +0200

    +

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { store } from './Redux/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <App />
       </Provider>
     </ThemeProvider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
