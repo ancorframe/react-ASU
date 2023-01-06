1fd560fdd2b057607915a0425b80007ece9df36f
ancorframe <yura.pundiak@gmail.com>
2023-01-06 21:30:15 +0200

    +

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/react-ASU/">
       <ThemeProvider theme={theme}>
         <GlobalStyle/>
             <App />
